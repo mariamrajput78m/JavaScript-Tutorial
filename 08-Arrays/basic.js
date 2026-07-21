51712c2175448c13f36566475595639c91881b25
Mariam's Github <228037552+mariamrajput78m@users.noreply.github.com>
2026-07-21 21:40:23 +0000

    Loop through Array

--- 08-Arrays/basic.js
+++ 08-Arrays/basic.js
@@ -13,11 +13,23 @@ const myArr2 = new Array(1, 2, 3, 4)
 // Array methods
 
 // myArr.push(6)
-// myArr.push(7)
-// myArr.pop()
+let fruits = ["Apple", "Banana"];
 
-// myArr.unshift(9)
-// myArr.shift()
+fruits.push("Orange");
+
+//console.log(fruits);
+
+// myArr.push(7)
+// myArr.pop()  //Removes the last element.
+
+// myArr.unshift(9) //Adds at the beginning.
+// myArr.shift() //Removes the first element.
+
+// Checking if smthg is in Array 
+let fruits = ["Apple", "Banana"];
+
+console.log(Array.isArray(fruits));
+
 
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
@@ -27,18 +39,32 @@ const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr);
 // console.log( newArr);
 
+//Convert Array to String
+
+let fruits = ["Apple","Banana","Orange"];
+
+console.log(fruits.join(" - "));
+
+//Loop through an Array
+
+let fruits = ["Apple" , "Banana" , "Mango"];
+
+for(let i = 0; i < fruits.length; i++)
+{
+    console.log(fruits[i]);
+}
 
 // slice, splice
 
-console.log("A ", myArr);
+// console.log("A ", myArr);
 
-const myn1 = myArr.slice(1, 3)
+// const myn1 = myArr.slice(1, 3)
 
-console.log(myn1);
-console.log("B ", myArr);
+// console.log(myn1);
+// console.log("B ", myArr);
 
 
-const myn2 = myArr.splice(1, 3)
-console.log("C ", myArr);
-console.log(myn2);
+// const myn2 = myArr.splice(1, 3)
+// console.log("C ", myArr);
+// console.log(myn2);
 
